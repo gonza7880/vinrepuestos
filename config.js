6c735c715895fe103c8a15dd235059e09511aafb
gonza7880 <gonzalo@scidata.com.ar>
2026-07-30 11:48:50 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// KALESI GROUP · Configuración de catálogo
+// Pegá acá la URL CSV publicada desde Google Sheets.
+// Ejemplo:
+// window.KALESI_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/XXXXXXXX/pub?gid=0&single=true&output=csv";
+
+window.KALESI_SHEET_CSV_URL = "";
