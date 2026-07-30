5cc6d6a404f85d2b3163c719681802019ba1e29d
gonza7880 <gonzalo@scidata.com.ar>
2026-07-30 11:55:30 -0300

    Actualizar URL de Google Sheets

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Ejemplo:
 // window.KALESI_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/XXXXXXXX/pub?gid=0&single=true&output=csv";
 
-window.KALESI_SHEET_CSV_URL = "";
+window.KALESI_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ18hlvPSZ0A3w-sMxZKeNAiW7T5U58pEaI5_uNiz8HxjDNWqVe1uea2apE2Om0-g/pub?output=csv";
